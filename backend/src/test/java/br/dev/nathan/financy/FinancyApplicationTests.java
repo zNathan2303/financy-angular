d601a3c68e2bfb7a95071ad5b73de80c9168ffba
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2026-03-19 21:25:26 -0300

    chore: adiciona Dockerfile

--- backend/src/test/java/br/dev/nathan/financy/FinancyApplicationTests.java
+++ backend/src/test/java/br/dev/nathan/financy/FinancyApplicationTests.java
@@ -2,8 +2,10 @@ package br.dev.nathan.financy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinancyApplicationTests {
 
 	@Test
